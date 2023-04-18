68b4fd6197633131d5edbf1a3b281ef16f464530
Ida Marie Andreassen <idaandreassen@Idas-MacBook-Pro-21.local>
2023-04-18 20:23:34 +0200

    Filtering gamma works, same EEGNet results as raw

--- pre_processing.py
+++ pre_processing.py
@@ -1,1 +1,61 @@
-#Implement SSP, 
+import numpy as np
+import mne
+import utils.variables as var
+from mne.filter import construct_iir_filter, filter_data, create_filter
+
+#Implement SSP, filtering etc.
+
+
+def create_raw_array(array):
+    '''
+    Creates a RawArray object
+
+    Parameters
+    ----------
+    array : ndarray
+        The array that will be converted into a RawArray
+
+    Returns
+    -------
+    raw_array : RawArray
+        An instance of a Raw object. Includes the data and info specified for the project. 
+
+    '''
+    info = mne.create_info(8, sfreq=var.SFREQ, ch_types='eeg', verbose=None)
+    raw_array = mne.io.RawArray(array, info)
+    mapping = {'0':'F4','1':'Fp2','2':'C3','3':'FC6','4':'O1','5':'Oz','6':'FT9','7':'T8'}
+    mne.rename_channels(raw_array.info, mapping)
+
+    return raw_array
+
+def filter_EEG(data, low_freq, high_freq):
+    '''
+    A function that filters the data, with a either bandpass, lowpass or highpass filters using the MNE function filter_data(..)
+
+    Parameters
+    ----------
+    data : dict
+        A dictionary containing the data. Shape (keys, number of channels, number of samples)
+    l_freq : int
+        The lower cutoff frequency given in Hz. If None then the filter will be low-pass.
+    h_freq : int
+        The upper cutoff frequency given in Hz. If None then the filter will be highpass. 
+
+    Returns
+    -------
+    filtered_data : dict
+        A dictionary with the filtered data with shape (keys, number of channels, number of samples)
+    '''
+    sfreq = var.SFREQ
+    filtered_data = {}
+    for key in data.keys():
+        data_for_filtering = data[key].copy().get_data()
+        filtering = filter_data(data_for_filtering, sfreq=sfreq, l_freq=low_freq, h_freq=high_freq)
+        filtered_raw_array = create_raw_array(filtering)
+        filtered_data[key]=filtered_raw_array
+    return filtered_data
+
+def decompose_EEG(data):
+    '''
+    
+    '''
--- prepare_data.py
+++ prepare_data.py
@@ -1,5 +1,5 @@
 from utils.data_processing import extract_eeg_data, segment_data, train_test_val_split, dict_to_arr
-from utils.labels import get_stai_labels, compute_stai_scores
+from utils.labels import get_stai_labels, compute_stai_scores, get_pss_labels
 from utils.valid_recordings import get_valid_recordings
 import numpy as np
 
@@ -10,7 +10,7 @@ def prepare_data(data_type, label_type, epoch_duration=5):
     Parameters
     ----------
     data_type : str
-        String containing the data type to be classified. Either 'raw' or 'filtered'
+        String containing the data type to be classified. Either 'raw', 'filtered', or 'decomposed'
     label_type : str
         Input can be either 'STAI' or 'PSS', to indicate either STAI-labels or PSS-labels.
     epoch_duration : int
@@ -32,39 +32,40 @@ def prepare_data(data_type, label_type, epoch_duration=5):
     val_labels : nd.array
         An array containing the validation labels of the given data type
     '''
+    valid_recordings = get_valid_recordings(data_type=data_type)
+    data = extract_eeg_data(valid_recordings=valid_recordings, data_type=data_type)
 
-    if data_type == 'raw' and label_type == 'STAI':
-
-        valid_recordings = get_valid_recordings(data_type)
-
-        labels           = get_stai_labels(valid_recordings, cutoff=40)
-        data             = extract_eeg_data(valid_recordings=valid_recordings, data_type=data_type)
-
-        segmented_data, segmented_labels = segment_data(data, labels, epoch_duration)
-
-        train_data_dict, train_labels_dict, test_data_dict, test_labels_dict, val_data_dict, val_labels_dict = train_test_val_split(segmented_data, segmented_labels)
-        
-        train_data      = dict_to_arr(train_data_dict)
-        test_data       = dict_to_arr(test_data_dict)
-        val_data        = dict_to_arr(val_data_dict)
-
-        train_labels    = np.reshape(np.array(list(train_labels_dict.values())), (len(train_data_dict),1))
-        test_labels     = np.reshape(np.array(list(test_labels_dict.values())), (len(test_data_dict),1))
-        val_labels      = np.reshape(np.array(list(val_labels_dict.values())), (len(val_data_dict),1))
-
-        print(f"Shape of train data set: {train_data.shape}")
-        print(f"Shape of train labels set: {train_labels.shape}")
-
-        print(f"Shape of test data set: {test_data.shape}")
-        print(f"Shape of test labels set: {test_labels.shape}")
-
-        print(f"Shape of validation data set: {val_data.shape}")
-        print(f"Shape of validation labels set: {val_labels.shape}")
-
-        return train_data, train_labels, test_data, test_labels, val_data, val_labels
-    
+    if label_type == 'STAI':
+        labels = get_stai_labels(valid_recordings, cutoff=40)
+    elif label_type == 'PSS':
+        labels = get_pss_labels(valid_recordings=valid_recordings, threshold=4)
     else:
-        return print('Should be filtered data here')
+        print('Suggested label_type is not supported.')
+
+    segmented_data, segmented_labels = segment_data(data, labels, epoch_duration)
+
+    train_data_dict, train_labels_dict, test_data_dict, test_labels_dict, val_data_dict, val_labels_dict = train_test_val_split(segmented_data, segmented_labels)
+    
+    train_data      = dict_to_arr(train_data_dict)
+    test_data       = dict_to_arr(test_data_dict)
+    val_data        = dict_to_arr(val_data_dict)
+
+    train_labels    = np.reshape(np.array(list(train_labels_dict.values())), (len(train_data_dict),1))
+    test_labels     = np.reshape(np.array(list(test_labels_dict.values())), (len(test_data_dict),1))
+    val_labels      = np.reshape(np.array(list(val_labels_dict.values())), (len(val_data_dict),1))
+
+    print(f"Shape of train data set: {train_data.shape}")
+    print(f"Shape of train labels set: {train_labels.shape}")
+
+    print(f"Shape of test data set: {test_data.shape}")
+    print(f"Shape of test labels set: {test_labels.shape}")
+
+    print(f"Shape of validation data set: {val_data.shape}")
+    print(f"Shape of validation labels set: {val_labels.shape}")
+
+    return train_data, train_labels, test_data, test_labels, val_data, val_labels
+    
+
 
 
 
