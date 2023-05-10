86f5f18f0ae4b86a1d901467671f525383c4bab0
Ida Marie Andreassen <idaandreassen@Idas-MacBook-Pro-21.local>
2023-05-10 16:22:37 +0200

    Fixed the plotting function for psd in Filtering

--- utils/variables.py
+++ utils/variables.py
@@ -16,6 +16,7 @@ NUM_RUNS = 2
 NUM_CHANNELS = 8
 SFREQ = 250
 MAPPING = {'0':'F4','1':'Fp2','2':'C3','3':'FC6','4':'O1','5':'Oz','6':'FT9','7':'T8'}
+CHANNELS = ['F4', 'Fp2', 'C3', 'FC6', 'O1', 'Oz', 'FT9', 'T8']
 
 NUM_SAMPLES = 75000
 EPOCH_LENGTH = 5
