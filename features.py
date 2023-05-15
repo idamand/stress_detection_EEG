f491cd01724dcdcd65ae6fff5aea17e71287148d
Ida Marie Andreassen <idaandreassen@Idas-MacBook-Pro-21.local>
2023-05-15 15:02:53 +0200

    Features are implemented

--- features.py
+++ features.py
@@ -18,33 +18,20 @@ def hjorth_features(data):
         list of ndarrays with the computed features.
     '''
 
-    #n_channels = var.NUM_CHANNELS
-    #features_per_channel = 2
-
-    #features = []
-    #for epoch in data:
-    #    n_trials           = len(epoch)
-    #    features_for_epoch = np.empty([n_trials, n_channels * features_per_channel])
-        
-    #    for channel, data in enumerate(epoch):
-    #       trial = epoch[data]
-    #        mobility_spect       = mnf.compute_hjorth_mobility_spect(var.SFREQ, trial)
-    #        complexity_spect     = mnf.compute_hjorth_complexity_spect(var.SFREQ, trial)
-    #        features_for_epoch[channel] = np.concatenate([mobility_spect, complexity_spect])
-
-    #    features_for_epoch = features_for_epoch.reshape([n_trials, n_channels*features_per_channel])
-    #    features.append(features_for_epoch)
-
+    sfreq = var.SFREQ
     features_per_channel = 2
-    n_epoch, n_channels, n_samples = data.shape 
-    features = np.empty([n_epoch, n_channels * features_per_channel])  
-    for i, epochs in enumerate(data):
-        for j, samples in enumerate(epochs):
-            mobility_spect       = mnf.compute_hjorth_mobility_spect(var.SFREQ, samples)
-            complexity_spect     = mnf.compute_hjorth_complexity_spect(var.SFREQ, samples)
-            print('Mob spect: ', mobility_spect)
-            features[i][j] = np.concatenate([mobility_spect, complexity_spect])
-    features = features.reshape([n_epoch, n_channels * features_per_channel])
+    n_recordings, n_channels, n_samples = data.shape
+
+    features = np.empty([n_recordings, n_channels*features_per_channel])
+
+    for i in range(n_recordings):
+        samples          = data[i]
+        mobility_spect   = mnf.compute_hjorth_mobility_spect(sfreq=sfreq, data=samples)
+        complexity_spect = mnf.compute_hjorth_complexity_spect(sfreq=sfreq, data=samples)
+        features[i]      = np.concatenate([mobility_spect, complexity_spect])
+
+    features = features.reshape([n_recordings, n_channels*features_per_channel])
+  
     return features
 
 def entropy_features(data):
@@ -62,23 +49,80 @@ def entropy_features(data):
         list of ndarrays of the computed features.
     '''
 
-    first_key = next(iter(data[0]))
-    n_channels, _ = data[0][first_key].shape
+    sfreq = var.SFREQ
     features_per_channel = 4
+    n_recordings, n_channels, n_samples = data.shape
 
-    features = []
-    for fold in data:
-        n_trials          = len(fold)
-        features_for_fold = np.empty([n_trials, n_channels * features_per_channel])
+    features = np.empty([n_recordings, n_channels*features_per_channel])
 
-        for j, key in enumerate(fold):
-            trial = fold[key]
-            app_entropy          = mnf.compute_app_entropy(trial)
-            samp_entropy         = mnf.compute_samp_entropy(trial)
-            spect_entropy        = mnf.compute_spect_entropy(var.SFREQ, trial)
-            svd_entropy          = mnf.compute_svd_entropy(trial)
-            features_for_fold[j] = np.concatenate([app_entropy, samp_entropy, spect_entropy, svd_entropy])
+    for i in range(n_recordings):
+        samples         = data[i]
+        app_entropy     = mnf.compute_app_entropy(data=samples)
+        samp_entropy    = mnf.compute_samp_entropy(data=samples)
+        spect_entropy   = mnf.compute_spect_entropy(sfreq=sfreq, data=samples)
+        svd_entropy     = mnf.compute_svd_entropy(data=samples)
+        features[i]     = np.concatenate([app_entropy, samp_entropy, spect_entropy, svd_entropy])
 
-        features_for_fold = features_for_fold.reshape([n_trials, n_channels*features_per_channel])
-        features.append(features_for_fold)
+    features = features.reshape([n_recordings, n_channels*features_per_channel])
+
     return features
+
+def time_series_features(data):
+    '''
+    Compute time-series features from data.
+
+    Parameters
+    ----------
+    data : ndarray, shape(n_recordings, n_channels, n_samples)
+        An ndarray containing the data.
+    
+    Returns
+    -------
+    features : ndarray, shape(n_recordings, n_channels*features_per_channel)
+        An ndarray of the computed features
+    '''
+    sfreq = var.SFREQ
+    features_per_channel = 4
+    n_recordings, n_channels, n_samples = data.shape
+
+    features = np.empty([n_recordings, n_channels*features_per_channel])
+
+    for i in range(n_recordings):
+        samples     = data[i]
+        ptp_amp     = mnf.compute_ptp_amp(data=samples)
+        variance    = mnf.compute_variance(data=samples)
+        rms         = mnf.compute_rms(data=samples)
+        mean        = mnf.compute_mean(data=samples)
+        features[i] = np.concatenate([ptp_amp, variance, rms, mean])
+
+    features = features.reshape([n_recordings, n_channels*features_per_channel])
+
+    return features
+
+def all_features(data):
+    '''
+    Compute hjorth, entropy and time-series features in one
+
+    '''
+
+    sfreq = var.SFREQ
+    features_per_channel = 10
+    n_recordings, n_channels, n_samples = data.shape
+
+    features = np.empty([n_recordings, n_channels*features_per_channel])
+
+    for i in range(n_recordings):
+        samples             = data[i]
+        mobility_spect      = mnf.compute_hjorth_mobility_spect(sfreq=sfreq, data=samples)
+        complexity_spect    = mnf.compute_hjorth_complexity_spect(sfreq=sfreq, data=samples)
+        app_entropy         = mnf.compute_app_entropy(data=samples)
+        samp_entropy        = mnf.compute_samp_entropy(data=samples)
+        spect_entropy       = mnf.compute_spect_entropy(sfreq=sfreq, data=samples)
+        svd_entropy         = mnf.compute_svd_entropy(data=samples)
+        ptp_amp             = mnf.compute_ptp_amp(data=samples)
+        variance            = mnf.compute_variance(data=samples)
+        rms                 = mnf.compute_rms(data=samples)
+        mean                = mnf.compute_mean(data=samples)
+        features[i]         = np.concatenate([mobility_spect, complexity_spect, app_entropy, samp_entropy, spect_entropy, svd_entropy, ptp_amp, variance, rms, mean])
+
+    features = features.reshape([n_recordings, n_channels*features_per_channel])
